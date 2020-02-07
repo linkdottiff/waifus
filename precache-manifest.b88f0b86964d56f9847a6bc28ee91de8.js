e66b40fe776d07143d9127aeca1283167652e5bb
Charlie <mlvenezuelan@gmail.com>
2020-02-07 15:28:09 -0400

    Updates

---
+++ precache-manifest.b88f0b86964d56f9847a6bc28ee91de8.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57e4f097ae5397e091785df4bc73aff0",
+    "url": "/waifus/index.html"
+  },
+  {
+    "revision": "768445952e4796058700",
+    "url": "/waifus/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "bc8286342467cff33a16",
+    "url": "/waifus/static/css/main.58121568.chunk.css"
+  },
+  {
+    "revision": "768445952e4796058700",
+    "url": "/waifus/static/js/2.01bde12f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/waifus/static/js/2.01bde12f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc8286342467cff33a16",
+    "url": "/waifus/static/js/main.75b7b1bd.chunk.js"
+  },
+  {
+    "revision": "dc160259c47fc64e4c57",
+    "url": "/waifus/static/js/runtime-main.9cf5d253.js"
+  },
+  {
+    "revision": "eeb6aa54c476b605915f4aaea82655e3",
+    "url": "/waifus/static/media/animeft.eeb6aa54.ttf"
+  },
+  {
+    "revision": "2c79c5e9754ecac71b65bcb3fada85d0",
+    "url": "/waifus/static/media/anko.2c79c5e9.jpg"
+  },
+  {
+    "revision": "a6fb5a86ba4499e1bce832deae0b59c0",
+    "url": "/waifus/static/media/harribel.a6fb5a86.jpg"
+  },
+  {
+    "revision": "70eaf73386d159f6013fd0a9a8e04614",
+    "url": "/waifus/static/media/ikumi.70eaf733.jpg"
+  },
+  {
+    "revision": "235bb5d66c0e2d9098446306be08fded",
+    "url": "/waifus/static/media/ino.235bb5d6.jpg"
+  },
+  {
+    "revision": "5a3d8b8a9626b925ac5b6e8b85152f48",
+    "url": "/waifus/static/media/katen.5a3d8b8a.png"
+  },
+  {
+    "revision": "f3092dc4bfd76eb0b71ed763381db035",
+    "url": "/waifus/static/media/konan.f3092dc4.jpg"
+  },
+  {
+    "revision": "1a4c4383db9614cc90580ab8ac459bc6",
+    "url": "/waifus/static/media/kukaku.1a4c4383.png"
+  },
+  {
+    "revision": "9a1b0119dee741fbd54a3753229a9b04",
+    "url": "/waifus/static/media/kurenai.9a1b0119.png"
+  },
+  {
+    "revision": "73dbc709669c2280ae1297fdf5c11843",
+    "url": "/waifus/static/media/mei terumi.73dbc709.jpg"
+  },
+  {
+    "revision": "103045a1b4b949190e7ba9bad29aea5d",
+    "url": "/waifus/static/media/nelliel.103045a1.png"
+  },
+  {
+    "revision": "f241d55a8e7891a7dce3e1e5edd3bfb8",
+    "url": "/waifus/static/media/orihime.f241d55a.jpg"
+  },
+  {
+    "revision": "9efb75579b50555aca8ceabba1b92fd5",
+    "url": "/waifus/static/media/rangiku.9efb7557.png"
+  },
+  {
+    "revision": "3a53b3579137b3270e59de5e51e41093",
+    "url": "/waifus/static/media/sakura.3a53b357.jpg"
+  },
+  {
+    "revision": "ff80ef664526014c113c337514b81e23",
+    "url": "/waifus/static/media/samui.ff80ef66.jpg"
+  },
+  {
+    "revision": "2412a5257d66326a2663b25df876fd85",
+    "url": "/waifus/static/media/temari.2412a525.jpg"
+  },
+  {
+    "revision": "1ea1601bf9860bd5904b1e6447a7d5db",
+    "url": "/waifus/static/media/tsunade.1ea1601b.png"
+  },
+  {
+    "revision": "4c7bea5efffe8accc7dcd2524f28efdf",
+    "url": "/waifus/static/media/yourichi.4c7bea5e.jpg"
+  }
+]);
